9588b5ce57903e1eef30481719c68683ea50effc
davide <davide30dedej@gmail.com>
2025-07-23 18:51:33 +0200

    refactor catch decorator to improve exception handling and logging; update type hints for clarity

--- jamstar/utils.py
+++ jamstar/utils.py
@@ -8,14 +8,17 @@ from loguru import logger
 from .models import LogLevel
 
 
-type R = dict[str, Any]
+logger.level(LogLevel.SILENT_EXC, no=15, color="<yellow>")
 
 
-def func_params[T, **P](func: Callable[P, T]) -> Callable[P, R]:
+type A = dict[str, Any]
+
+
+def func_params[T, **P](func: Callable[P, T]) -> Callable[P, A]:
     sig = signature(func)
 
     @wraps(func_params)
-    def inner(*args: P.args, **kwargs: P.kwargs) -> R:
+    def inner(*args: P.args, **kwargs: P.kwargs) -> A:
         bound_args = sig.bind(*args, **kwargs)
         bound_args.apply_defaults()
         return dict(bound_args.arguments)
@@ -23,30 +26,42 @@ def func_params[T, **P](func: Callable[P, T]) -> Callable[P, R]:
     return inner
 
 
-def catch[T, **P](
+type R[T, D] = T | D | None
+
+
+def catch[T, D, V, **P](
     level: str = LogLevel.SILENT_EXC,
-    default: T | None = None,
+    exc_type: type[BaseException] = Exception,
+    reraise: bool = False,
+    log_traceback: bool = False,
+    default: D | None = None,
     message: str | None = None,
-) -> Callable[[Callable[P, T]], Callable[P, T | None]]:
-    def decorator(func: Callable[P, T]) -> Callable[P, T | None]:
+) -> Callable[[Callable[P, T]], Callable[P, R[T, D]]]:
+    def decorator(func: Callable[P, T]) -> Callable[P, R[T, D]]:
         @wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T | None:
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R[T, D]:
             try:
                 return func(*args, **kwargs)
-            except Exception as e:
-                if not message:
-                    return default
+            except exc_type as e:
+                if message:
+                    format_dict: dict[str, Any] = {
+                        **func_params(func)(*args, **kwargs),
+                        "function_name": func.__name__,
+                        "exception": repr(e),
+                    }
+                    try:
+                        log_message = message.format(**format_dict)
+                    except KeyError as ke:
+                        log_message = (
+                            f"Invalid template for {func.__name__}: {message} (missing: {ke}). Exception: {e!r}"
+                        )
+                    logger.log(level, log_message)
 
-                format_dict: dict[str, Any] = {
-                    **func_params(func)(*args, **kwargs),
-                    "function_name": func.__name__,
-                    "exception": repr(e),
-                }
-                try:
-                    log_message = message.format(**format_dict)
-                except KeyError as ke:
-                    log_message = f"Invalid template for {func.__name__}: {message} (missing: {ke}). Exception: {e!r}"
-                logger.log(level, log_message)
+                if reraise:
+                    raise
+
+                if log_traceback:
+                    logger.exception(f"Exception in {func.__name__}: {e!r}")
 
                 return default
 
